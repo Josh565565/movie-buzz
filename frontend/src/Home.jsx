d142945f08f0155922a8304fa77ca70149170a08
Josh565565 <smartcash565@gmail.com>
2024-06-04 15:12:34 +0100

    updated Home screen

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Nav from "./components/home/Nav";
+import SideNav from "./components/home/SideNav";
 
 const Home = () => {
   return (
     <>
       <Nav />
+      <div className="p-6 text-lg">
+        <p className="pb-8">Welcome Josh, to Your Personal Movie Tracker!</p>
+        <SideNav />
+      </div>
     </>
   );
 };
